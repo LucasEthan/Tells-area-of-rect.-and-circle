b9fe9aae30a030ac2da055f734e4bc4300e14f1a
Lucas Ethan <asmrit1010@gmail.com>
2020-08-26 10:30:31 +0200

    Simplified and cleaned the code

--- geometry.rb
+++ geometry.rb
@@ -1,27 +1,28 @@
 #!/usr/bin/env ruby
 print "Do you want to get the area of a [r]ectangle or a [c]ircle? "
 choice = gets.chomp
-
+# Initialize area
+area = 0.0
 if choice == "r"
   # Asks user for length
   print "What is the length of the rectangle (in meters ?) ".chomp
-  length = gets
+  length = gets.to_f
   # Asks user for width
   print "What is the width of the rectangle (in meters ?) "
-  width = gets
+  width = gets.to_f
   # calculates and says the area of the rectangle
-  area = "#{length.to_f * width.to_f}"
-  puts(format("The area is  %.2f m, bye!", "#{area}"))
+  area = length * width
+  
 
-else if choice == "c"
+elsif choice == "c"
   # Asks the user for radius
   print "What is the radius of the circle (in meters)? "
-  radius = gets
+  radius = gets.to_f
   # calculates and says the area of the circle
-  area_2 = "#{radius.to_f * radius.to_f * 3.14159265359.to_f}"
-  puts(format("The area is  %.2f m, bye!", "#{area_2}"))
+  area = radius * radius * 3.14159265359
+  
 else
   # Choice must be either "r" or "c"
   puts "You have entered an invalid choice."
 end
-end
+puts(format("The area is  %.2f m, bye!", area))
