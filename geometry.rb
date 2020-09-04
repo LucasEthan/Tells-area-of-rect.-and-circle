c462501b63f544dd3e26649eee0df1c0eea8e8a5
Lucas Ethan <asmrit1010@gmail.com>
2020-09-04 12:01:42 +0200

    It gives you an error if u enter a invalid radius

--- geometry.rb
+++ geometry.rb
@@ -2,7 +2,7 @@
 
 require_relative "rectangle"
 require_relative "circle"
-
+begin
 loop do
 print "Do you want to calculate a [r]ectangle or a [c]ircle? "
 choice = gets.chomp
@@ -16,22 +16,25 @@ diameter = 0.0
 if choice == "r"
   # Asks user for length
   print "What is the length of the rectangle (in meters ?) ".chomp
-  length = gets.to_f
+  length = Integer(gets)
   # Asks user for width
   print "What is the width of the rectangle (in meters ?) "
-  width = gets.to_f
+  width = Integer(gets)
   rectangle = Rectangle.new(length, width)
   area = rectangle.area
   perimeter = rectangle.perimeter
-  
+    
 elsif choice == "c"
   # Asks the user for radius
   print "What is the radius of the circle (in meters)? "
-  radius = gets.to_f
-  circle = Circle.new(radius)
-  area = circle.area
-  diameter = circle.diameter
-  circumference = circle.circumference
+  if radius = Integer(gets) > 0
+    circle = Circle.new(radius)
+    area = circle.area
+    diameter = circle.diameter
+    circumference = circle.circumference
+  else
+    puts "You have entered a invalid input"
+  end
 else
   puts "You have entered a invalid input"
 end
@@ -45,4 +48,7 @@ print "Do you want to quit yes or no? [y/n]: "
 choice = gets.chomp.upcase
 
 break if choice == "Y"
+end
+rescue ArgumentError => e
+  puts "You have entered an invalid input"
 end
