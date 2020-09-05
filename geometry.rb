f81a49da99be532c1f8baccb04cda6f77cb6478b
Lucas Ethan <asmrit1010@gmail.com>
2020-09-05 10:34:41 +0200

    Added Guard clause and removed file

--- geometry.rb
+++ geometry.rb
@@ -2,54 +2,61 @@
 
 require_relative "rectangle"
 require_relative "circle"
+
+def calculate_for_rectangle
+  # Asks user for length and width 
+  print "What is the length of the rectangle (in meters ?) "
+  length = Integer(gets)
+
+  print "What is the width of the rectangle (in meters ?) "
+  width = Integer(gets)
+
+  # Guard clause
+  unless length.positive? && width.positive?
+    puts "Length and width must be positive"
+    return
+  end
+
+    rectangle = Rectangle.new(length, width)
+    area = rectangle.area
+    perimeter = rectangle.perimeter
+
+  puts(format("The area is %.2f m", area))
+  puts(format("The perimeter is %.2f m", perimeter))
+end
+
+def calculate_for_circle
+  # Asks the user for radius
+  print "What is the radius of the circle (in meters)? "
+  radius = Integer(gets)
+
+  # Guard clause
+  unless radius.positive?
+    puts "Radius must be positive"
+    return
+  end
+
+  circle = Circle.new(radius)
+  area = circle.area
+  diameter = circle.diameter
+  circumference = circle.circumference 
+  
+  puts(format("The diameter is %.2f m", diameter)) 
+  puts(format("The circumference is %.2f m", circumference))
+end
+
 begin
   loop do
     print "Do you want to calculate a [r]ectangle or a [c]ircle? "
     choice = gets.chomp
 
-    # Initialize variables
-    area = 0.0
-    perimeter = 0.0
-    circumference = 0.0
-    diameter = 0.0
-
-    if choice == "r"
-      # Asks user for length and width
-      print "What is the length of the rectangle (in meters ?) "
-      length = Integer(gets)
-
-      print "What is the width of the rectangle (in meters ?) "
-      width = Integer(gets)
-
-      if length.positive? && width.positive?     
-        rectangle = Rectangle.new(length, width)
-        area = rectangle.area
-        perimeter = rectangle.perimeter
-          
-      else
-        puts "Length and width must be positive"
-      end
-
-
+    if choice == "r" 
+      calculate_for_rectangle
     elsif choice == "c"
-      # Asks the user for radius
-      print "What is the radius of the circle (in meters)? "
-      radius = Integer(gets)
-
-      if radius.positive?
-        circle = Circle.new(radius)
-        area = circle.area
-        diameter = circle.diameter
-        circumference = circle.circumference 
-      else
-        puts "You have entered a invalid input"
-      end
+      calculate_for_circle
+    else
+      puts "You must enter either r or c "
     end
-
-    puts(format("The area is %.2f m", area)) unless area.zero?
-    puts(format("The perimeter is %.2f m", perimeter)) unless perimeter.zero?
-    puts(format("The diameter is %.2f m", diameter)) unless diameter.zero?
-    puts(format("The circumference is %.2f m", circumference)) unless circumference.zero?
 
     print "Do you want to quit yes or no? [y/n]: "
     choice = gets.chomp.upcase
